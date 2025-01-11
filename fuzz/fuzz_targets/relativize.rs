c9bfa2f3ce02aeb839903261cad79b5bc278a897
Thomas Tanon <thomas@pellissier-tanon.fr>
2025-01-11 16:23:27 +0100

    Fuzz: also take for input the base IRI

--- fuzz/fuzz_targets/relativize.rs
+++ fuzz/fuzz_targets/relativize.rs
@@ -1,44 +1,38 @@
 #![no_main]
+
 use libfuzzer_sys::fuzz_target;
 use oxiri::Iri;
 use std::str;
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(s) = str::from_utf8(data) {
-        let Ok(iri) = Iri::parse(s) else {
-            return;
-        };
-        for base in [
-            "http://a/b/c/d?q",
-            "http://a/",
-            "http://a",
-            "http:",
-            "http:/",
-            "http:/a",
-            "http:/a/b",
-            "http:/a/",
-            "http:a",
-            "http:a/b",
-            "http:?a",
-            "http:#a",
-            "http://a?b",
-            "http:/a?b",
-            "http:a?b",
-        ] {
-            let base = Iri::parse_unchecked(base);
-            match base.relativize(&iri) {
-                Ok(relative) => {
-                    let from_relative = base.resolve(relative.as_str()).unwrap();
-                    assert_eq!(
-                        iri, from_relative,
-                        "Resolving {relative} computed from {iri} with base {base} gives {from_relative}"
-                    );
-                }
-                Err(_) => {
-                    // We make sure it's not possible to relativize
-                    assert_ne!(base.resolve(&iri.as_str()).unwrap(), iri);
-                }
-            }
+    let parts = data.split(|c| *c == b'\0').collect::<Vec<_>>();
+    let Ok([absolute, base]) = <[&[u8]; 2]>::try_from(parts) else {
+        return;
+    };
+    let Ok(absolute) = str::from_utf8(absolute) else {
+        return;
+    };
+    let Ok(absolute) = Iri::parse(absolute) else {
+        return;
+    };
+    let Ok(base) = str::from_utf8(base) else {
+        return;
+    };
+    let Ok(base) = Iri::parse(base) else {
+        return;
+    };
+    let base = Iri::parse_unchecked(base);
+    match base.relativize(&absolute) {
+        Ok(relative) => {
+            let from_relative = base.resolve(relative.as_str()).unwrap();
+            assert_eq!(
+                absolute, from_relative,
+                "Resolving {relative} computed from {absolute} with base {base} gives {from_relative}"
+            );
+        }
+        Err(_) => {
+            // We make sure it's not possible to relativize
+            assert_ne!(base.resolve(&absolute.as_str()).unwrap(), absolute);
         }
     }
 });
--- fuzz/fuzz_targets/resolve.rs
+++ fuzz/fuzz_targets/resolve.rs
@@ -4,12 +4,21 @@ use oxiri::Iri;
 use std::str;
 
 fuzz_target!(|data: &[u8]| {
-    let Ok(s) = str::from_utf8(data) else {
+    let parts = data.split(|c| *c == b'\0').collect::<Vec<_>>();
+    let Ok([relative, base]) = <[&[u8]; 2]>::try_from(parts) else {
         return;
     };
-    let base = Iri::parse("http://a/b/c/d;p?q").unwrap();
-    let unchecked = base.resolve_unchecked(s);
-    let Ok(valid) = base.resolve(s) else {
+    let Ok(relative) = str::from_utf8(relative) else {
+        return;
+    };
+    let Ok(base) = str::from_utf8(base) else {
+        return;
+    };
+    let Ok(base) = Iri::parse(base) else {
+        return;
+    };
+    let unchecked = base.resolve_unchecked(relative);
+    let Ok(valid) = base.resolve(relative) else {
         return;
     };
     // We check that unchecked resolving gives the same result
